f080eb8df17ab44da504cd587a1dcf1eccc66008
Stan Skrivanek <s.skrivanek@icloud.com>
2020-02-03 20:57:09 +0000

    code upto lesson 10

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class Navbar extends Component {
+  // set default props
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github"
+  };
+  // make sure that property have always proper type (string, boolean, number etc.)
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
+
+  render() {
+    return (
+      <nav className="navbar bg-primary">
+        <h1>
+          <i className={this.props.icon}></i> {this.props.title}
+        </h1>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
