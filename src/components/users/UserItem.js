00f0ebfd7e799e0968449cfd7937cfb5c3cb0302
Stan Skrivanek <s.skrivanek@icloud.com>
2020-02-03 21:56:03 +0000

    deleting constructor and destructuring properties from this.state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,28 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: "id",
-      login: "mojombo",
-      avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-      html_url: "https://github.com/mojombo"
-    };
-  }
+  // We have deleted constructor an using only state declaration
+  state = {
+    id: "id",
+    login: "mojombo",
+    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+    html_url: "https://github.com/mojombo"
+  };
+
   render() {
+    //   we have also destructured properties from props object. Now we dont need to use this.state where were originally used
+    const { login, avatar_url, html_url } = this.state;
     return (
       <div className="card text-center">
         <img
-          src={this.state.avatar_url}
+          src={avatar_url}
           alt="avatar"
           className="round-img"
           style={{ width: "60px" }}
         />
-        <h3>{this.state.login}</h3>
+        <h3>{login}</h3>
         <div>
-          <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
             more
           </a>
         </div>
