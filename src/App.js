f5ed6992ed456fcc5ba663f13255a0c450d3cfbb
Stan Skrivanek <s.skrivanek@icloud.com>
2020-02-03 21:50:17 +0000

    sadd tate in constructor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, Component } from "react";
 import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 import "./App.css";
 import "./assets/fa/css/all.min.css";
 
@@ -10,8 +11,10 @@ class App extends Component {
         <Fragment>
           {/* this component will get default props (Navbar.js)  */}
           <Navbar />
+
           {/* or we can ad them directly */}
           {/* <Navbar title="Github Finder" icon="fab fa-github" /> */}
+          <UserItem />
         </Fragment>
       </div>
     );
