07bce45a966ee00a69a4894fe02bd4a8fd51ea9c
Stan Skrivanek <s.skrivanek@icloud.com>
2020-02-04 23:18:39 +0000

    REFACTORED Navbar, users and UserItem to functional components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,27 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-export class Navbar extends Component {
-  // set default props
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github"
-  };
-  // make sure that property have always proper type (string, boolean, number etc.)
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
-
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon}></i> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
-
+// Refactored to functional component
+const Navbar = props => {
+  // console.log(props);
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        {/* no need keyword `this` as this component is now function */}
+        <i className={props.icon}></i> {props.title}
+      </h1>
+    </nav>
+  );
+};
+// set default `props` OUTSIDE func. component
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github"
+};
+// set default `types` OUTSIDE func. component
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,29 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-  // We have deleted constructor an using only state declaration
-  // and also deleted state
-  render() {
-    //   we have also destructured properties from props object. Now we don't need to use this.state where were originally used
-    const { login, avatar_url, html_url } = this.props.user;
-    // this.state deleted (we have no state anymore ). Instead using props (`this.props.user`) passed from Users.js
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt="avatar"
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            more
-          </a>
-        </div>
+// Refactored to functional component
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  // properties are now destructured
+  // BEFORE destructuring
+  // const UserItem = (props) => {
+  // const { login, avatar_url, html_url } = props.user;
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt="avatar"
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          more
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+// add propTypes
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
