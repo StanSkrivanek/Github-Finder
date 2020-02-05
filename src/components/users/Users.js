7666bf76f15223b10a706a0ee0bd9ac67fad6a5f
Stan Skrivanek <s.skrivanek@icloud.com>
2020-02-05 00:11:17 +0000

    Add Spiner.js, proptypes in Users.js

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,21 +1,33 @@
 import React, { Component } from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  render() {
+// Destructured props
+const Users = ({ users, loading }) => {
+  // add condition
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
-// another way how to add style to component (instead inline) ia store style in variable and pass this variable as value of `style` attribute
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
+
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3,1fr)",
   gridGap: "1rem"
 };
+
 export default Users;
