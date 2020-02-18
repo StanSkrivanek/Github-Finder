df1bc4395bc7fcbec346aa169cb5d7ad5a98a84a
Stan Skrivanek <s.skrivanek@icloud.com>
2020-02-18 22:59:56 +0000

    Navbar Fix

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,31 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-<<<<<<< HEAD
-export class Navbar extends Component {
-  // set default props
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github"
-  };
-  // make sure that property have always proper data type (string, boolean, number etc.)
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
-
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon}></i> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
-
-=======
 // Refactored to functional component
 const Navbar = props => {
   // console.log(props);
@@ -57,5 +32,5 @@ Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired
 };
->>>>>>> deployy_01_lesson_35
+
 export default Navbar;
