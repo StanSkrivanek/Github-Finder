2a667c1261657705817ebf2702d2d7b138bc59d5
Stan Skrivanek <s.skrivanek@icloud.com>
2020-02-18 20:42:44 +0000

    Mooving Repos to Context (App, GithubState, User, Search, githubReducer)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
-import axios from "axios";
+// import axios from "axios";
 import GithubState from "./context/github/GithubState";
 
 import "./App.css";
@@ -15,19 +15,9 @@ import "./assets/fa/css/all.min.css";
 const App = () => {
   // const [users, setUsers] = useState([]);
   // const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // const [repos, setRepos] = useState([]);
+  // const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
-
-  const getUserRepos = async username => {
-    // this.setState({ loading: true });
-    setLoading(true);
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setRepos(res.data);
-    setLoading(false);
-  };
 
   const showAlert = (msg, type) => {
     setAlert({ msg, type });
@@ -49,31 +39,14 @@ const App = () => {
                   path="/"
                   render={props => (
                     <Fragment>
-                      <Search
-                        // searchUsers={searchUsers}
-                        // clearUsers={clearUsers}
-                        // showClear={users.length > 0 ? true : false}
-                        showAlert={showAlert}
-                      />
+                      <Search showAlert={showAlert} />
                       <Users />
                     </Fragment>
                   )}
                 />
                 <Route exact path="/about" component={About} />
-                <Route
-                  exact
-                  path="/user/:login"
-                  render={props => (
-                    <User
-                      {...props}
-                      // getUser={getUser}
-                      getUserRepos={getUserRepos}
-                      // user={user}
-                      repos={repos}
-                      // loading={loading}
-                    />
-                  )}
-                />
+                <Route exact path="/user/:login" component={User} />
+                )} />
               </Switch>
             </div>
           </Fragment>
